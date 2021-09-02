9a0fdbbd53c439c9c7371d7ef27a3c1bfc883e5f
tessla-muir <tesslamuir@yahoo.com>
2021-09-02 00:41:14 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './header/top-nav-bar.component';
+import { SideNavBarComponent } from './header/side-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    SideNavBarComponent
   ],
   imports: [
     BrowserModule
