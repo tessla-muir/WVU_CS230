8be9eec26ec6262738aa0bd218d4c9c861097765
tessla-muir <tesslamuir@yahoo.com>
2021-09-09 00:09:59 -0400

    Added Livestream Carousel and Cards

---
+++ src/app/main/livestream-cards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-livestream-cards',
+    templateUrl: 'livestream-cards.component.html',
+    styleUrls :['livestream-cards.component.css']
+})
+export class LiveSteamCards {
+
+}
