ef0f8a8dabe3890788558a8a365b1582b5a4beca
tessla-muir <tesslamuir@yahoo.com>
2021-09-30 11:45:11 -0400

    Procedural generation of livestream-cards in remaining pages

--- src/app/body/browse_page/browse_routes/browse-livechannels.component.ts
+++ src/app/body/browse_page/browse_routes/browse-livechannels.component.ts
@@ -1,9 +1,19 @@
 import { Component } from "@angular/core";
+import { LivestreamCard } from "src/app/main/livestream-cards.model";
+import { mock_livestreamcards } from "src/app/main/mock-livestream-cards";
 
 @Component({
     selector: 'app-browse-livechannels',
     templateUrl: 'browse-livechannels.component.html',
+    styleUrls: ['browse-livechannels.component.css']
 })
 export class BrowsePage_LiveChannelsComponent {
+    
+    livestreamcards:LivestreamCard[] = [];
 
+    constructor() {
+        for (var livestreamcard of mock_livestreamcards) {
+            this.livestreamcards.push(new LivestreamCard(livestreamcard));
+        }
+    }
 }
--- src/app/layout/home_layout/layout-home.component.ts
+++ src/app/layout/home_layout/layout-home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { LivestreamCard } from "src/app/main/livestream-cards.model";
+import { mock_livestreamcards } from "src/app/main/mock-livestream-cards";
 
 @Component ({
     selector: "app-layout-home",
@@ -7,4 +9,11 @@ import { Component } from "@angular/core";
 })
 export class LayoutHomeComponent {
 
+    livestreamcards:LivestreamCard[] = [];
+
+    constructor() {
+        for (var livestreamcard of mock_livestreamcards) {
+            this.livestreamcards.push(new LivestreamCard(livestreamcard));
+        }
+    }
 }
