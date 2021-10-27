ece18bf973bdf6cee65a33fea830870d2be9314f
tessla-muir <tesslamuir@yahoo.com>
2021-10-26 22:01:03 -0400

    Added counter to put for category-cards.service

--- src/app/main/category-cards.service.ts
+++ src/app/main/category-cards.service.ts
@@ -2,19 +2,22 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { CategoryCard } from "./category-cards.model";
 
+
 @Injectable({providedIn: 'root'})
 export class CategoryCardsService {
     private baseUrl:string = "https://twitch-app-9f7b9-default-rtdb.firebaseio.com/";
-    private categoryCardsEndpoints = 'categories.json';
+    private categoryCardsEndpoints = 'categories';
+    private counter:number = 6;
     constructor(private http: HttpClient) {
 
     }
 
     getCategoryCard() {
-        return this.http.get<CategoryCard []>(this.baseUrl + this.categoryCardsEndpoints);
+        return this.http.get<CategoryCard []>(this.baseUrl + this.categoryCardsEndpoints + ".json");
     }
 
     modifyCategoryCard(data:CategoryCard) {
-        return this.http.post(this.baseUrl + this.categoryCardsEndpoints, data);
+        this.counter++;
+        return this.http.put(this.baseUrl + this.categoryCardsEndpoints + "/" + this.counter + ".json", data);
     }
 }
